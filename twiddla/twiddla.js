5a51b717fe5ff922bb9f153915517fade850d4a8
Avital Oliver <avital@asana.com>
2011-06-09 19:51:19 -0700

    Added deploy script (see Asana for password) and updated twiddla.js to use setTimeout

--- twiddla/twiddla.js
+++ twiddla/twiddla.js
@@ -1,9 +1,10 @@
-window.onload = function() {
+// TODO: Find the proper event to fire on rather than use setTimeout
+setTimeout(function() {
   document.getElementById('divSideband').style.display = '';
 
   sendSideband = function(type) {
     document.getElementById('txtChat').value = ':: ' + type + ' ::';
     sendChat();
   };
-};
+}, 5000);
 
