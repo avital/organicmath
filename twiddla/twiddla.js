e5b7b1ebf539b265e1baaf353edaa49c436daee4
Avital Oliver <avital@asana.com>
2011-06-09 20:58:31 -0700

    A bunch of stuff works

--- twiddla/twiddla.js
+++ twiddla/twiddla.js
@@ -8,3 +8,38 @@ setTimeout(function() {
   };
 }, 5000);
 
+// TODO: Eliminate setTimeout!
+setTimeout(function() {
+  originalAppendChatBox = TChat.AppendChatBox;
+  TChat.AppendChatBox = 
+      function(container, user, text, allowHTML, includeUserIcon, className) {
+    if (text.substring(0, 2) === '@@') {
+      var command = text.substring(2);
+      window[command](user);
+    }
+
+    originalAppendChatBox(
+        container, user, text, allowHTML, includeUserIcon, className);
+  };
+
+  originalAppendUserRow = TChat.AppendUserRow;
+  TChat.AppendUserRow = function(container, user) {
+    originalAppendUserRow(container, user);
+    var user_div = container.lastChild;
+    if (user.is_typing) {
+      user_div.style.backgroundColor = '#287593';
+    } else {
+      user_div.style.backgroundColor = '';
+    }
+  };
+}, 8000);
+
+starttyping = function(user) {
+  user.is_typing = true;
+  RefreshUsers();
+};
+
+stoptyping = function(user) {
+  user.is_typing = false;
+  RefreshUsers();
+};
